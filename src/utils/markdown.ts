6b497943a1a5fb1b136d2e1e677ee2fa56ca2106
Jonathan Robinson <jonrobinson.codes@gmail.com>
2024-10-07 23:46:40 -0400

    feat: add markdown to html function

--- src/utils/markdown.ts
+++ src/utils/markdown.ts
@@ -1,18 +1,35 @@
-const mkdownToHtml = (markdown: string) => {
-  markdown = markdown.replace(/# (.*?)(\r\n|\r|\n)/g, '<h1>$1</h1>');
-  markdown = markdown.replace(/## (.*?)(\r\n|\r|\n)/g, '<h2>$1</h2>');
-  markdown = markdown.replace(/### (.*?)(\r\n|\r|\n)/g, '<h3>$1</h3>');
+const mkdownToHtml = (
+  markdown: string,
+  options: { addLineBreaks?: boolean } = {}
+) => {
+  markdown = markdown.replace(/### (.*?)(\r\n|\r|\n|$)/g, '<h3>$1</h3>$2');
+  markdown = markdown.replace(/## (.*?)(\r\n|\r|\n|$)/g, '<h2>$1</h2>$2');
+  markdown = markdown.replace(/# (.*?)(\r\n|\r|\n|$)/g, '<h1>$1</h1>$2');
   markdown = markdown.replace(/\*\*(.*?)\*\*/g, '<strong>$1</strong>');
   markdown = markdown.replace(/\*(.*?)\*/g, '<em>$1</em>');
   markdown = markdown.replace(/\[(.*?)\]\((.*?)\)/g, '<a href="$2">$1</a>');
-  markdown = markdown.replace(/^([A-Za-z].*?)$/gm, '<p>$1</p>');
 
-  //   markdown = toParagraphHtml(markdown);
+  markdown = toParagraphHtml(markdown, options);
   return markdown.trim();
 };
 
-const toParagraphHtml = (content: string) => {
-  const lines = content.split('\n');
+const toParagraphHtml = (
+  content: string,
+  opitions: { addLineBreaks?: boolean } = {}
+) => {
+  const contenlines = content.split('\n');
+  const lines = [];
+
+  for (let i = 0; i < contenlines.length; i++) {
+    let current = contenlines[i];
+    if (!!lines[lines.length - 1] && !!contenlines[i]) {
+      current = lines.pop() || '';
+      const lineBreakTag = opitions.addLineBreaks ? '<br>' : ' ';
+      current += lineBreakTag + contenlines[i];
+    }
+
+    lines.push(current);
+  }
 
   const tagRegex = /^<.*>.*<\/.*>$/;
   const startWordRegex = /^[A-Za-z].*$/;
--- tests/utils/markdown.test.ts
+++ tests/utils/markdown.test.ts
@@ -1,6 +1,8 @@
 // sum.test.js
 import { expect, test, describe } from 'vitest';
+import * as fs from 'fs';
 import { mkdownToHtml, toParagraphHtml } from '../../src/utils/markdown.js';
+import path from 'path';
 
 describe('toParagraphHtml', () => {
   test('string conversion', () => {
@@ -20,27 +22,93 @@ describe('toParagraphHtml', () => {
   });
 
   test('string with multi line conversion', () => {
-    const content = `Markdown was created by John Gruber with the help of Aaron Swartz to offer a simple way to write web content. 
-      The idea was to make writing in plain text readable while also being convertible to HTML.`;
-    const expected = `<p>Markdown was created by John Gruber with the help of Aaron Swartz to offer a simple way to write web content.</p>\n<p>The idea was to make writing in plain text readable while also being convertible to HTML.</p>`;
+    const content = `Markdown was created by John Gruber with the help of Aaron Swartz to offer a simple way to write web content.
+The idea was to make writing in plain text readable while also being convertible to HTML.`;
+    const expected = `<p>Markdown was created by John Gruber with the help of Aaron Swartz to offer a simple way to write web content. The idea was to make writing in plain text readable while also being convertible to HTML.</p>`;
     const actual = toParagraphHtml(`${content}`);
+    expect(actual).toBe(expected);
+  });
+
+  test('string with multi line conversion and line breaks', () => {
+    const content = `Markdown was created by John Gruber with the help of Aaron Swartz to offer a simple way to write web content.
+The idea was to make writing in plain text readable while also being convertible to HTML.`;
+    const expected = `<p>Markdown was created by John Gruber with the help of Aaron Swartz to offer a simple way to write web content.<br>The idea was to make writing in plain text readable while also being convertible to HTML.</p>`;
+    const actual = toParagraphHtml(`${content}`, { addLineBreaks: true });
     expect(actual).toBe(expected);
   });
 });
 
 describe('mkdownToHtml', () => {
-  test('string conversion', () => {
-    const content = `# Sample Document
-
-Hello!
-
-This is sample markdown for the [Mailchimp](https://www.mailchimp.com) homework assignment.`;
+  test('Sample1 Data Text', () => {
+    const sample1 = fs.readFileSync(
+      path.resolve(__dirname, '../mock/files/sample1.txt'),
+      'utf8'
+    );
 
     const expected = `<h1>Sample Document</h1>
+
 <p>Hello!</p>
 
 <p>This is sample markdown for the <a href="https://www.mailchimp.com">Mailchimp</a> homework assignment.</p>`;
-    const actual = mkdownToHtml(content);
-    expect(actual).toBe(expected);
+
+    const sample1HtmlActual = mkdownToHtml(sample1);
+    fs.writeFile(
+      path.resolve(__dirname, '../mock/files/sample1Output.txt'),
+      sample1HtmlActual,
+      (err) => {}
+    );
+    expect(sample1HtmlActual).toBe(expected);
+  });
+
+  test('Sample2 Data Text without Line Breaks', () => {
+    const sample2 = fs.readFileSync(
+      path.resolve(__dirname, '../mock/files/sample2.txt'),
+      'utf8'
+    );
+
+    const sample2HtmlActual = mkdownToHtml(sample2);
+    const expected = `<h1>Header one</h1>
+
+<p>Hello there</p>
+
+<p>How are you? What's going on?</p>
+
+<h2>Another Header</h2>
+
+<p>This is a paragraph <a href="http://google.com">with an inline link</a>. Neat, eh?</p>
+
+<h2>This is a header <a href="http://yahoo.com">with a link</a></h2>`;
+    fs.writeFile(
+      path.resolve(__dirname, '../mock/files/sample2WithoutOutput.txt'),
+      sample2HtmlActual,
+      (err) => {}
+    );
+    expect(sample2HtmlActual).toBe(expected);
+  });
+
+  test('Sample2 Data Text with Line Breaks', () => {
+    const sample2 = fs.readFileSync(
+      path.resolve(__dirname, '../mock/files/sample2.txt'),
+      'utf8'
+    );
+
+    const sample2HtmlActual = mkdownToHtml(sample2, { addLineBreaks: true });
+    const expected = `<h1>Header one</h1>
+
+<p>Hello there</p>
+
+<p>How are you?<br>What's going on?</p>
+
+<h2>Another Header</h2>
+
+<p>This is a paragraph <a href="http://google.com">with an inline link</a>. Neat, eh?</p>
+
+<h2>This is a header <a href="http://yahoo.com">with a link</a></h2>`;
+    fs.writeFile(
+      path.resolve(__dirname, '../mock/files/sample2WithLineBreaksOutput.txt'),
+      sample2HtmlActual,
+      (err) => {}
+    );
+    expect(sample2HtmlActual).toBe(expected);
   });
 });
